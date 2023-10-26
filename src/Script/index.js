d815ab9d8c08df0af09569dc953ed9b9f1463468
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-26 16:05:46 +0530

    style : some style

--- src/Script/index.js
+++ src/Script/index.js
@@ -3,38 +3,34 @@ const hour = document.getElementById("hours");
 const minute = document.getElementById("minutes");
 const second = document.getElementById("seconds");
 
-var ampm = document.getElementById("ampm");
+var ampm = document.querySelector("#ampm");
 
 function updateClock()
 {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s =  new Date().getSeconds();
-
-    let ampm = "AM";
+    let pmam = ampm;
 
     if(h>12)
     {
         h = h-12;
-        ampm = "PM";
-    } else{
-        ampm = "AM";
+        pmam = 'PM';
     }
     
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
- 
-    hour.innerText = h;
-    minute.innerText = m;
-    second.innerText = s;
-    ampm.innerText = ampm;
-   
-    setTimeout(() => {
-        updateClock()
-    }, 1000);
     
-    // ampm,(innerText==ampm);
+ 
+    hour.innerHTML = h;
+    minute.innerHTML = m;
+    second.innerText = s;
+    ampm.innerHTML = pmam
+
+    setTimeout(() => {
+        updateClock();
+    }, 1000)
 }
 
 updateClock();
