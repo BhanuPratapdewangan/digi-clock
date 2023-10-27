a2816cb833ea0f79400dcec0404d6ea0e4e4f8cb
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-27 12:38:44 +0530

    fix : logic building

--- src/Script/index.js
+++ src/Script/index.js
@@ -5,28 +5,27 @@ const second = document.getElementById("seconds");
 
 var ampm = document.querySelector("#ampm");
 
-function updateClock()
-{
+function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
-    let s =  new Date().getSeconds();
+    let s = new Date().getSeconds();
+
     let pmam = ampm;
 
-    if(h>12)
-    {
-        h = h-12;
+    if (h >= 12) {
+        h = h - 12;
         pmam = 'PM';
     }
-    
+
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
-    
- 
+
+
     hour.innerHTML = h;
     minute.innerHTML = m;
     second.innerText = s;
-    ampm.innerHTML = pmam
+    ampm.innerHTML= pmam;
 
     setTimeout(() => {
         updateClock();
